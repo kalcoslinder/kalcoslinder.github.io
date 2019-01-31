a04154c1284f57777df5c73b5cd974028d62a633
kalcoslinder <44510117+kalcoslinder@users.noreply.github.com>
2019-01-31 14:40:17 -0800

    Update top.php

--- top.php
+++ top.php
@@ -11,8 +11,3 @@
 		<title>kalcoslinder portfolio</title>
 		<link rel = "stylesheet" type = "text/css" href = "/css/main.css" >
 		<script src="processing.js"></script>
-		<script type="application/processing" data-processing-target="pjs">
-		/* @pjs globalKeyEvents="true"; */ 
-		void setup() {
-		  size(400, 400);
-		}
