fb4f7d5dc05fcf85e7c4af7a6aed5d571ff5b824
kalcoslinder <44510117+kalcoslinder@users.noreply.github.com>
2019-01-31 14:29:29 -0800

    Update top.php

--- top.php
+++ top.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 	<head>
 		kalcoslinder portfolio
 		<header>
@@ -6,31 +8,11 @@
 		</ul>
 			
 		</header>
+		<title>kalcoslinder portfolio</title>
 		<link rel = "stylesheet" type = "text/css" href = "/css/main.css" >
 		<script src="processing.js"></script>
 		<script type="application/processing" data-processing-target="pjs">
-		void setup(){
-			size(200,200);
+		/* @pjs globalKeyEvents="true"; */ 
+		void setup() {
+		  size(400, 400);
 		}
-		background(140,140,140);
-		
-		</script>
-		<title>kalcoslinder portfolio</title>
-	</head>
-	<body>
-		<canvas data-processing-sources="you-win.pde"></canvas>
-		
-		<a href="you-win.html"><img src = "images/you-win.png" width = "200" height = "200" /></a>
-		
-		<a href="tree.html"><img src = "images/tree.png" width = "200" height = "200" /></a>
-		
-		
-		
-		<footer>
-    		<ul>
-        		<li><a href="mailto:christopherwerry@gmail.com">email</a></li>
-			<li><a href="https://www.khanacademy.org/profile/crustyoucantrust/projects">khanacademy</a></li>
-        		<li><a href="https://github.com/kalcoslinder/kalcoslinder.github.io">github</a></li>
-			</ul>
-		</footer>
-	</body>
