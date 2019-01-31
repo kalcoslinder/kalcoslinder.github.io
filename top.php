800a6f6b3459b95f3b278806362a00b4a4a0a4fa
kalcoslinder <44510117+kalcoslinder@users.noreply.github.com>
2019-01-31 13:56:34 -0800

    Add files via upload

---
+++ top.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		kalcoslinder portfolio
+		<header>
+		<ul>
+			
+		</ul>
+			
+		</header>
+		<link rel = "stylesheet" type = "text/css" href = "/css/main.css" >
+		<script src="processing.js"></script>
+		<script type="application/processing" data-processing-target="pjs">
+		void setup(){
+			size(200,200);
+		}
+		background(140,140,140);
+		
+		</script>
+		<title>kalcoslinder portfolio</title>
+	</head>
+	<body>
+		<canvas data-processing-sources="you-win.pde"></canvas>
+		
+		<a href="you-win.html"><img src = "images/you-win.png" width = "200" height = "200" /></a>
+		
+		<a href="tree.html"><img src = "images/tree.png" width = "200" height = "200" /></a>
+		
+		
+		
+		<footer>
+    		<ul>
+        		<li><a href="mailto:christopherwerry@gmail.com">email</a></li>
+			<li><a href="https://www.khanacademy.org/profile/crustyoucantrust/projects">khanacademy</a></li>
+        		<li><a href="https://github.com/kalcoslinder/kalcoslinder.github.io">github</a></li>
+			</ul>
+		</footer>
